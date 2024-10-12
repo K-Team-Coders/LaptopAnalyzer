0210a8c9c19ee53c83dce691f77d9f706e4aec68
Your Carma <y0urcarma@yandex.ru>
2024-10-12 18:47:38 +0300

    frontend: Training Component Added

---
+++ frontend/src/views/Training.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="h-full p-2">
+      <div
+        class="outline-1 outline shadow-[0_0px_5px_2px_rgba(0,0,0,0.1)] rounded-[5px] h-full flex flex-col"
+      >
+        Дообучение
+      </div>
+    </div>
+  </template>
+  
+  <script setup></script>
+  
